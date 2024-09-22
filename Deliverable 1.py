76448a07275cc204b92a3f0578c476b15f80dea0
fadi <fadihaider23@gmail.com>
2024-09-22 13:25:23 -0600

    Create Deliverable 1.py

---
+++ Deliverable 1.py
@@ -0,0 +1,15 @@
+# Delivarable 1.py 
+# ENDG 319 CURE Project Delivarable 1
+# Group 158
+# September 22, 2024
+# Group Members:
+# Fadi Haider
+# Kaleb Chhoa 
+# Yahya Elmadhoun
+# Youssef Abdalla
+
+def main():
+    main()
+
+if __name__ == "__main__":
+    main()
