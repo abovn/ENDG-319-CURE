1fd01a992b4ebe1d7bca3cc66ce7b03cffcf9239
fadi <fadihaider23@gmail.com>
2024-09-27 13:08:43 -0600

    Update Task 4.py

--- Task 4.py
+++ Task 4.py
@@ -1,6 +1,9 @@
 # Task 4 - Kaleb Chhoa
+import numpy as np
 from sklearn.datasets import load_digits, load_breast_cancer, load_digits
-digit_datasets = load_digits()
+digit_dataset = load_digits()
 breast_dataset = load_breast_cancer()
 
-print(digit_datasets.keys)
+print(digit_dataset.keys())
+
+print(digit_dataset.DESCR)
