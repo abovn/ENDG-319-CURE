748b2c00b6576e4267eb0382cb0bfff937519497
KChh0a <halotogo70@gmail.com>
2024-09-27 12:45:14 -0600

    Imported sklearn

--- Task 4.py
+++ Task 4.py
@@ -1,1 +1,6 @@
 # Task 4 - Kaleb Chhoa
+from sklearn.datasets import load_digits, load_breast_cancer, load_digits
+digit_datasets = load_digits()
+breast_dataset = load_breast_cancer()
+
+print(digit_datasets.keys)
