14018a45c12178c0d4bbf3f309a72e394afc4630
KChh0a <halotogo70@gmail.com>
2024-09-27 13:26:45 -0600

    cimmit message

--- Task 4.py
+++ Task 4.py
@@ -1,6 +1,19 @@
 # Task 4 - Kaleb Chhoa
-import numpy as np
-from sklearn.datasets import load_digits, load_breast_cancer, load_digits
+from sklearn.datasets import load_digits, load_breast_cancer
 digit_dataset = load_digits()
 breast_dataset = load_breast_cancer()
 
+print(digit_dataset.keys())
+# print(digit_dataset.DESCR)
+
+digit_dataset_lines = digit_dataset.DESCR.splitlines()
+selected_lines = digit_dataset_lines[7:13]
+
+for line in selected_lines:
+    print(line)
+
+selected_lines = digit_dataset_lines[17:20]
+
+for line in selected_lines:
+     print(line)
+    
