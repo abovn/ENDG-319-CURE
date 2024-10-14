fb3e2246599610b4d0da990579b810dd870be77f
fadi <fadihaider23@gmail.com>
2024-10-13 21:11:52 -0600

    Finalized Task 4 with 5 per variable

--- Deliverable 2/D2 T4.py
+++ Deliverable 2/D2 T4.py
@@ -6,9 +6,9 @@ import pandas as pd
 
 # Task 4: Create a custom dataset for vehicle classification
 data_vehicleclass = {
-    'engine_size': [3.5, 2.4, 2.4, 2, 3.3, 3.5, 3.0],
-    'fuel_efficiency': [10.5, 8.7, 9.8, 7.5, 11.9, 8.1, 12.4],
-    'vehicle_type': ['SUV','Sedan','Hatchback','Sedan','Minivan','Sedan','SUV']
+    'engine_size': [2.4,1.6,2.4,4.8,2,2.4,3.5,2.7,3.8,3.6,4,4.8,2.7,6.5,3.5],
+    'fuel_efficiency': [9.9,9.4,10,14,11.8,13.5,12.7,10.5,14.3,16.4,15.3,13.3,11.9,19,10.5],
+    'vehicle_type': ['Sedan','Sedan','Sedan','Sedan','Sedan','Minivan','Minivan','Minivan','Minivan','Minivan','SUV','SUV','SUV','SUV','SUV',]
 }
 
 # Convert the data into a dataframe
